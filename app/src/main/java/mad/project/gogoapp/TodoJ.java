08cfdb90b4b2ee0c335fe443c1166a07d1a726a4
Sanduni Sathsarani <99157473+IT21266546@users.noreply.github.com>
2023-05-01 21:59:26 +0530

    Initially TodoJ file added

---
+++ app/src/main/java/mad/project/gogoapp/TodoJ.java
@@ -0,0 +1,72 @@
+package mad.project.gogoapp;
+
+public class TodoJ {
+
+    private String id;
+    private String title;
+    private String time;
+    private String date;
+    private String location;
+    private double payment;
+
+    public TodoJ(){
+
+    }
+
+    public TodoJ(String title,String time,String date, String location,Double payment) {
+        this.title=title;
+        this.time=time;
+        this.date=date;
+        this.location=location;
+        this.payment=payment;
+    }
+
+    public TodoJ(String id,String title,String time,String date, String location,Double payment) {
+        this.id=id;
+        this.title=title;
+        this.time=time;
+        this.date=date;
+        this.location=location;
+        this.payment=payment;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getTitle() { return title; }
+
+    public void setTitle(String title) { this.title = title; }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getPayment() {
+        return payment;
+    }
+
+    public void setPayment(double payment) {
+        this.payment = payment;
+    }
+
+    public String getId() { return id; }
+
+    public void setId(String id) { this.id = id; }
+}
