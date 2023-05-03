a2b908b1772a748c56827f22d8c04d7932e08c8c
Sanduni Sathsarani <99157473+IT21266546@users.noreply.github.com>
2023-05-03 12:29:59 +0530

    Todo.kt added

--- app/src/main/java/mad/project/gogoapp/Todo1.kt
+++ app/src/main/java/mad/project/gogoapp/Todo1.kt
@@ -1,72 +1,35 @@
-package mad.project.gogoapp;
+package mad.project.gogoapp
 
-public class Todo {
+class Todo {
+    var id: String? = null
+    var title: String? = null
+    var time: String? = null
+    var date: String? = null
+    var location: String? = null
+    var payment = 0.0
 
-    private String id;
-    private String title;
-    private String time;
-    private String date;
-    private String location;
-    private double payment;
-
-    public Todo(){
-
+    constructor() {}
+    constructor(title: String?, time: String?, date: String?, location: String?, payment: Double) {
+        this.title = title
+        this.time = time
+        this.date = date
+        this.location = location
+        this.payment = payment
     }
 
-    public Todo(String title, String time, String date, String location, Double payment) {
-        this.title=title;
-        this.time=time;
-        this.date=date;
-        this.location=location;
-        this.payment=payment;
+    constructor(
+        id: String?,
+        title: String?,
+        time: String?,
+        date: String?,
+        location: String?,
+        payment: Double
+    ) {
+        this.id = id
+        this.title = title
+        this.time = time
+        this.date = date
+        this.location = location
+        this.payment = payment
     }
-
-    public Todo(String id, String title, String time, String date, String location, Double payment) {
-        this.id=id;
-        this.title=title;
-        this.time=time;
-        this.date=date;
-        this.location=location;
-        this.payment=payment;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public String getTitle() { return title; }
-
-    public void setTitle(String title) { this.title = title; }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public double getPayment() {
-        return payment;
-    }
-
-    public void setPayment(double payment) {
-        this.payment = payment;
-    }
-
-    public String getId() { return id; }
-
-    public void setId(String id) { this.id = id; }
 }
