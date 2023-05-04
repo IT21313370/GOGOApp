7e13a9d086fd39048ae5ce6563a46603f6c46d1f
Sanduni Sathsarani <99157473+IT21266546@users.noreply.github.com>
2023-05-04 16:43:02 +0530

    change again todoclass kotlin to TodoClass.java. data added to the databse. No issues in the code

---
+++ app/src/main/java/mad/project/gogoapp/TodoClass.java
@@ -0,0 +1,72 @@
+package mad.project.gogoapp;
+
+public class TodoClass {
+
+    private String id;
+    private String title;
+    private String time;
+    private String date;
+    private String location;
+    private double payment;
+
+    public TodoClass (){
+
+    }
+
+    public TodoClass (String title,String time,String date, String location,Double payment) {
+        this.title=title;
+        this.time=time;
+        this.date=date;
+        this.location=location;
+        this.payment=payment;
+    }
+
+    public TodoClass (String id,String title,String time,String date, String location,Double payment) {
+        this.id=id;
+        this.title=title;
+        this.time=time;
+        this.date=date;
+        this.location=location;
+        this.payment=payment;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getTitle() { return title; }
+
+    public void setTitle(String title) { this.title = title; }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getPayment() {
+        return payment;
+    }
+
+    public void setPayment(double payment) {
+        this.payment = payment;
+    }
+
+    public String getId() { return id; }
+
+    public void setId(String id) { this.id = id; }
+}
